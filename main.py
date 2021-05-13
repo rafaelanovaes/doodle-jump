1f1e45be430aa9084fb2aec81fcd9d25584e12d6
rafaelanovaes <novaes.rafa14@gmail.com>
2021-05-13 18:23:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import pygame
 import os
-import sys
 import random
+import sys
+import pygame
 
 WIN_WIDTH = 400
 WIN_HEIGHT = 533
 
-BG_IMG = pygame.image.load(os.path.join("images", "background.png"))
-DOODLE_IMG = pygame.image.load(os.path.join("images", "doodle.png"))
-PLAT_IMG = pygame.image.load(os.path.join("images", "platform.png"))
+BG_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "Cenario PyGame.png"))
+DOODLE_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "DoodleEngineer.png"))
+PLAT_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "plat.png"))
+
 
 class Plat:
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("Doodle Jump!")
+pygame.display.set_caption("Doodle Engineer Jump!")
 
 pygame.font.init()
 SCORE_FONT = pygame.font.SysFont("comicsans", 30)
 
-plates = [Plat(random.randrange(0, WIN_WIDTH), random.randrange(0, WIN_HEIGHT)) for i in range(15)]
+plates = [Plat(random.randrange(0, WIN_WIDTH), random.randrange(0, WIN_HEIGHT)) for i in range(10)]
 
 x = 100
 y = 100
@@ -30,47 +32,47 @@ h = 200
 score = 0
 
 while True:
-	events = pygame.event.get()
-	for event in events:
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-	
-	win.blit(BG_IMG, (0, 0))
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-	for plat in plates:
-		win.blit(PLAT_IMG, (plat.x, plat.y))
-	
-	keys = pygame.key.get_pressed()
-	if keys[pygame.K_LEFT]:
-		x -= 4
-		DOODLE_IMG = pygame.image.load(os.path.join("images", "doodle.png"))
-	if keys[pygame.K_RIGHT]:
-		x += 4
-		DOODLE_IMG = pygame.transform.flip(pygame.image.load(os.path.join("images", "doodle.png")), True, False)
+    win.blit(BG_IMG, (0, 0))
 
-	if y < h:
-		y = h
-		for plat in plates:
-			plat.y = plat.y - dy
-			if plat.y > WIN_HEIGHT:
-				plat.y = 0
-				plat.x = random.randrange(0, WIN_WIDTH)
-				score += 1
-				print(score)
+    for plat in plates:
+        win.blit(PLAT_IMG, (plat.x, plat.y))
 
-	dy += 0.2
-	y += dy
-	if y > WIN_HEIGHT:
-		dy = -10
-	
-	for plat in plates:
-		if (x + 50  > plat.x) and (x + 20 < plat.x + 68) and (y + 70 > plat.y) and (y + 70 < plat.y + 14) and dy > 0:
-			dy = -10
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= 4
+        DOODLE_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "DoodleEngineer.png"))
+    if keys[pygame.K_RIGHT]:
+        x += 4
+        DOODLE_IMG = pygame.transform.flip(pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "DoodleEngineer.png")), True, False)
 
-	text = SCORE_FONT.render("Score: " + str(score), 1, (0, 0, 0))
-	win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+    if y < h:
+        y = h
+        for plat in plates:
+            plat.y = plat.y - dy
+            if plat.y > WIN_HEIGHT:
+                plat.y = 0
+                plat.x = random.randrange(0, WIN_WIDTH)
+                score += 1
+                print(score)
 
-	win.blit(DOODLE_IMG, (x, y))
-	
-	pygame.display.update()
+    dy += 0.2
+    y += dy
+    if y > WIN_HEIGHT:
+        dy = -6
+
+    for plat in plates:
+        if (x + 50 > plat.x) and (x + 20 < plat.x + 150) and (y + 70 > plat.y) and (y + 70 < plat.y + 14) and dy > 0:
+            dy = -10
+
+    text = SCORE_FONT.render("Score: " + str(score), 1, (0, 0, 0))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 25))
+
+    win.blit(DOODLE_IMG, (x, y))
+
+    pygame.display.update()
