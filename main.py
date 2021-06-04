599874aeba58391c01f765ceb54211bb7471a73f
rafaelanovaes <novaes.rafa14@gmail.com>
2021-06-04 13:17:59 -0300

    Add changes

--- main.py
+++ main.py
@@ -1,55 +1,99 @@
+#Importando as bibliotecas necessárias utilizadas no código
 import os
 import random
 import sys
 import pygame
 
+#Definindo o tamanho da tela do jogo
 WIN_WIDTH = 400
 WIN_HEIGHT = 533
 
-BG_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "Cenario PyGame.png"))
-DOODLE_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "DoodleEngineer.png"))
-PLAT_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "plat.png"))
+#Carregando as imagens pro jogo
+BG_IMG = pygame.image.load(os.path.join("images", "cenarioPyGame.png"))
+DOODLE_IMG = pygame.image.load(os.path.join("images", "DoodleEngineer.png"))
+PLAT_IMG = pygame.image.load(os.path.join("images", "Plat_4.png"))
 
-
+#Definindo as plataformas em função para facilitar a repetição das mesmas
 class Plat:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+#Tela de inicio do game
+def show_go_screen():
+    win.blit(BG_IMG, (0, 0))
+    draw_text(win, "DOODLE ENGINEER GAME!", 40, WIN_WIDTH / 2, WIN_HEIGHT / 4)
+    draw_text(win, "Use as setas do teclado para se movimentar ", 22, WIN_WIDTH / 2, WIN_HEIGHT / 2)
+    draw_text(win, "para a direita ou esquerda.", 22, WIN_WIDTH / 2, WIN_HEIGHT / 1.9)
+    draw_text(win, "Pressione qualquer tecla para começar!", 25, WIN_WIDTH / 2, WIN_HEIGHT * 3 /4)
+    pygame.display.flip()
+    wainting = True
+    while wainting:
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                wainting = False
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, GREEN)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
+#Colocando o nome do jogo no topo da tela
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Doodle Engineer Jump!")
 
+#Definindo a letra e tamanho do placar de pontuação e tela de inicio e game over
 pygame.font.init()
 SCORE_FONT = pygame.font.SysFont("comicsans", 30)
+font_name = pygame.font.match_font('VT323')
 
-plates = [Plat(random.randrange(0, WIN_WIDTH), random.randrange(0, WIN_HEIGHT)) for i in range(10)]
+#Função que gera as plataformas de forma aleatória
+plates = [Plat(random.randrange(0, WIN_WIDTH), random.randrange(0, WIN_HEIGHT - 4)) for i in range(7)]
 
-x = 100
-y = 100
-dy = 0.0
-h = 200
-score = 0
+#Inicializando variáveis
+x = 100                  #Dimensão dos objetos
+y = 100                  #Dimensão dos objetos
+dy = 0.0                 #Impulso doodle (2 tipos)
+h = 200                  #Altura
+score = 0                #Pontuação
+game_over = True         #Tela de inicio ou fim
+GREEN = (0, 180, 0)      #Define cor
 
+#Inicio do loop do jogo
 while True:
-    events = pygame.event.get()
-    for event in events:
+    #Introduzindo tela inicial e atualizando o display
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    if game_over :
+        show_go_screen()
+        game_over = False
+    event = pygame.event.get()
 
+    # Checando os cantos da tela (Doodle consegue passar de um lado para o outro)
+    if x < 0:
+        x = 400
+    if x > 400:
+        x = 0
+
+    #blit = Basicamente copia os pixels de uma tela na outra, nesse caso ele pega os pixels da sprite(imagem importada) e copia na tela do jogo
     win.blit(BG_IMG, (0, 0))
-
+    #introduzindo a sprite da plataforma dentro da função plates (define as dimensões da plataforma)
     for plat in plates:
         win.blit(PLAT_IMG, (plat.x, plat.y))
-
+    #Definindo teclas utilizadas no jogo (para direita e esquerda) e quanto o doodle irá ser deslocado
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
-        x -= 4
-        DOODLE_IMG = pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "DoodleEngineer.png"))
+        x -= 3
+        DOODLE_IMG = pygame.image.load(os.path.join("images", "DoodleEngineer.png"))
     if keys[pygame.K_RIGHT]:
-        x += 4
-        DOODLE_IMG = pygame.transform.flip(pygame.image.load(os.path.join("F:\.Faculdade\pythonGAME", "DoodleEngineer.png")), True, False)
+        x += 3
+        #Fazendo o flip na imagem do doodle para que quando a tecla for pressionada a sprite vire também
+        DOODLE_IMG = pygame.transform.flip(pygame.image.load(os.path.join("images", "DoodleEngineer.png")), True, False)
 
     if y < h:
         y = h
@@ -64,15 +108,19 @@ while True:
     dy += 0.2
     y += dy
     if y > WIN_HEIGHT:
-        dy = -6
+        dy = -1           #Impulso de colisão com o chão
+    for event in event:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
+    #Definindo dimensões de colisão da plataforma com o doodle
     for plat in plates:
-        if (x + 50 > plat.x) and (x + 20 < plat.x + 150) and (y + 70 > plat.y) and (y + 70 < plat.y + 14) and dy > 0:
-            dy = -10
+        if (x + 40 > plat.x) and (x + 40 < plat.x + 150) and (y + 80 > plat.y) and (y + 70 < plat.y + 14) and dy > 0:
+            dy = -10      #Impulso de colisão com a plataforma
 
     text = SCORE_FONT.render("Score: " + str(score), 1, (0, 0, 0))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 25))
-
     win.blit(DOODLE_IMG, (x, y))
 
     pygame.display.update()
